7d362fa05dc45a0d0050b7aa46dd3fe80e1d7e16
Cristian Tosco <ctosco@tres42.com.ar>
2014-05-07 19:35:02 -0300

    Update config.js
    agregado de prefix a config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window._skel_config={preset:"standard",prefix:"css/style",resetCSS:!0,breakpoints:{"1000px":{grid:{gutters:25}}}},window._skel_panels_config={preset:"standard"};
+window._skel_config={preset:"standard",prefix:"las-artes/css/style",resetCSS:!0,breakpoints:{"1000px":{grid:{gutters:25}}}},window._skel_panels_config={preset:"standard"};
